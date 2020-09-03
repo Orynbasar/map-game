9b739661e8fb731e906cfdaad5c38908391566af
Onggar <ongarorynbasar@mail.ru>
2020-09-03 10:43:43 +0600

    add defenders spot init function

--- scripts/vscripts/game/game.lua
+++ scripts/vscripts/game/game.lua
@@ -14,11 +14,32 @@ function Game.init()
     end
 end
 
+function Game.initDefenderTable()
+    for _, playerID in ipairs(Game.playersPool) do
+        DefenderTable[playerID] = {}
+
+        local playerSpotPosition = SpotList:getSpotByPlayerID(playerID).position
+
+        local xStep = MAP_SPOT_X_LENGTH / (DEFENDERS_COUNT_X + 1)
+        local yStep = MAP_DEFENDERS_Y_LENGTH / (DEFENDERS_COUNT_Y + 1)
+
+        local defendersSpotCenter = playerSpotPosition + Vector(0, MAP_SPOT_CENTER_TO_DEFENDER_Y_LENGTH, 0)
+        local defendersSpotFirstEdge = defendersSpotCenter - Vector(MAP_SPOT_X_LENGTH / 2, -MAP_WAVE_Y_LENGTH / 2, 0)
+
+        for x = 1, DEFENDERS_COUNT_X do
+            DefenderTable[playerID][x] = {}
+            for y = 1, DEFENDERS_COUNT_Y do
+                DefenderTable[playerID][x][y] = { playerSpot = defendersSpotFirstEdge + Vector(xStep * x, -yStep * y, 0) }
+            end
+        end
+    end
+end
+
 function Game.start()
     Timers:CreateTimer(1, function()
         for _, playerID in ipairs(Game.playersPool) do
-            Wave({ { unitCount = 5, unitName = 'npc_dota_creep_badguys_ranged', frontLinePriority = 20 } ,
-                   { unitCount = 5, unitName = 'npc_dota_badguys_siege', frontLinePriority = 10 }   }, playerID):spawnWave()
+            Wave({ { unitCount = 5, unitName = 'npc_dota_creep_badguys_ranged', frontLinePriority = 20 },
+                   { unitCount = 5, unitName = 'npc_dota_badguys_siege', frontLinePriority = 10 } }, playerID):spawnWave()
         end
 
         return 5000.0
