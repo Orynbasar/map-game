ad8c463680dba5ce96ed4be154e0d9b2ee9d739f
Onggar <ongarorynbasar@mail.ru>
2020-09-23 03:36:56 +0600

    remove unused ability

--- scripts/vscripts/abilities/slark_fighter/hero_remove.lua
+++
@@ -1,6 +0,0 @@
-hero_remove = class({})
-LinkLuaModifier( "modifier_hero_remove_lua", "modifiers/modifier_hero_remove_lua", LUA_MODIFIER_MOTION_NONE )
-
-function hero_remove:GetIntrinsicModifierName()
-    return "modifier_hero_remove_lua"
-end
